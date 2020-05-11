92d9ec96f38e31fabb2f39fd101512b153e8d53a
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-10 22:55:57 -0400

    Update weather_dataset.py

--- weather_dataset.py
+++ weather_dataset.py
@@ -1,1 +1,153 @@
+""" box plotting """
+# Read 'weather.csv' into a DataFrame named 'weather'
+weather = pd.read_csv('weather.csv')
+
+# Describe the temperature columns
+print(weather[['TMIN', 'TAVG', 'TMAX']].describe())
+
+# Create a box plot of the temperature columns
+weather[['TMIN', 'TAVG', 'TMAX']].plot(kind='box')
+
+# Display the plot
+plt.show()
+
+
+""" hist plotting """
+# Create a 'TDIFF' column that represents temperature difference
+weather['TDIFF'] = weather['TMAX'] - weather['TMIN']
+
+# Describe the 'TDIFF' column
+print(weather['TDIFF'].describe())
+
+# Create a histogram with 20 bins to visualize 'TDIFF'
+weather['TDIFF'].plot(kind = 'hist', bins = 20)
+
+# Display the plot
+plt.show()
+
+
+""" Counting bad weather conditions
+The weather DataFrame contains 20 columns that start with 'WT', each of which represents a bad weather condition. For example:
+
+WT05 indicates "Hail"
+WT11 indicates "High or damaging winds"
+WT17 indicates "Freezing rain"
+For every row in the dataset, each WT column contains either a 1 (meaning the condition was present that day) 
+or NaN (meaning the condition was not present).
+
+In this exercise, you'll quantify "how bad" the weather was each day by counting the number of 1 values in each row.
+"""
+# Copy 'WT01' through 'WT22' to a new DataFrame
+WT = weather.loc[:, 'WT01':'WT22']
+
+# Calculate the sum of each row in 'WT'
+weather['bad_conditions'] = WT.sum(axis = 'columns')
+
+# Replace missing values in 'bad_conditions' with '0'
+weather['bad_conditions'] = weather.bad_conditions.fillna(0).astype('int')
+
+# Create a histogram to visualize 'bad_conditions'
+weather['bad_conditions'].plot(kind='hist')
+
+# Display the plot
+plt.show()
+
+
+""" Mapping the weather conditions """
+# Count the unique values in 'bad_conditions' and sort the index
+print(weather.bad_conditions.value_counts().sort_index())
+
+# Create a dictionary that maps integers to strings
+mapping = {0:'good', 1:'bad', 2:'bad', 3:'bad', 4:'bad', 5:'worse', 6:'worse', 7:'worse', 8:'worse', 9:'worse'}
+
+# Convert the 'bad_conditions' integers to strings using the 'mapping'
+weather['rating'] = weather.bad_conditions.map(mapping)
+
+# Count the unique values in 'rating'
+print(weather['rating'].value_counts())
+
+
+""" Changing the data type to ordered category """
+# Create a list of weather ratings in logical order
+cats = ['good', 'bad', 'worse']
+
+# Change the data type of 'rating' to category
+weather['rating'] = weather.rating.astype('category', ordered=True, categories=cats)
+
+# Examine the head of 'rating'
+print(weather.rating.head())
+
+
+
+""" Merge """
+# Reset the index of 'ri'
+ri.reset_index('stop_datetime', inplace=True)
+
+# Examine the head of 'ri'
+print(ri.head())
+
+# Create a DataFrame from the 'DATE' and 'rating' columns
+weather_rating = weather[['DATE', 'rating']]
+
+# Examine the head of 'weather_rating'
+print(weather_rating.head())
+
+# Examine the shape of 'ri'
+print(ri.shape)
+
+# Merge 'ri' and 'weather_rating' using a left join
+ri_weather = pd.merge(left=ri, right=weather_rating, left_on='stop_date', right_on='DATE', how='left')
+
+# Examine the shape of 'ri_weather'
+print(ri_weather.shape)
+
+# Set 'stop_datetime' as the index of 'ri_weather'
+ri_weather.set_index('stop_datetime', inplace=True)
+
+
+""" Does weather affect the arrest rate? """
+# Calculate the overall arrest rate
+print(ri_weather.is_arrested.mean())
+
+# Calculate the arrest rate for each 'rating'
+print(ri_weather.groupby('rating').is_arrested.mean())
+
+# Calculate the arrest rate for each 'violation' and 'rating'
+print(ri_weather.groupby(['violation', 'rating']).is_arrested.mean())
+
+
+
+""" Selecting from a multi-indexed Series
+The output of a single .groupby() operation on multiple columns is a Series with a MultiIndex. 
+Working with this type of object is similar to working with a DataFrame:
+
+The outer index level is like the DataFrame rows.
+The inner index level is like the DataFrame columns.
+
+You can select a single value from the Series as follows: 
+series_name.loc['outer index level', 'inner index level'].
+
+You can select a row from the Series as follows: 
+series_name.loc['outer index level'].
+"""
+# Save the output of the groupby operation from the last exercise
+arrest_rate = ri_weather.groupby(['violation', 'rating']).is_arrested.mean()
+
+# Print the 'arrest_rate' Series
+print(arrest_rate)
+
+# Print the arrest rate for moving violations in bad weather
+print(arrest_rate.loc['Moving violation', 'bad'])
+
+# Print the arrest rates for speeding violations in all three weather conditions
+print(arrest_rate.loc['Speeding'])
+
+
+
+""" To reshape a multi-indexed Series into a DataFrame, use the .unstack() method. """
+# Unstack the 'arrest_rate' Series into a DataFrame
+print(arrest_rate.unstack())
+
+# Create the same DataFrame using a pivot table
+print(ri_weather.pivot_table(index='violation', columns='rating', values='is_arrested'))
 
